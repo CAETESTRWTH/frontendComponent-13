446f1797f9c9876d0d29054a105b5a9387076a1f
erdzan12 <jungerjunge2@gmail.com>
2021-03-24 01:42:55 +0000

    MicroserviceCall edited by Erdzan Rastoder

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -66,7 +66,9 @@ var getpoemsagain = function(){
 
   client.sendRequest("GET", "poemstwo", "", "", {}, false,
   function(data, type) {
-    console.log(data);
+    console.log(data); 
+    document.getElementById("p1").innerText = data.map(song => song.title);
+
   },
   function(error) {
     console.log(error);
